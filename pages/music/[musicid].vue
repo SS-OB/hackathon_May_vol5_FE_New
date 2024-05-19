3a9b7072e16469c0b6ce164b8d062878514aa9cf
Fujiken0705 <kentofujita.new@gmail.com>
2024-05-19 10:54:13 +0900

    CSSあてました

--- pages/music/[musicid].vue
+++ pages/music/[musicid].vue
@@ -1,5 +1,10 @@
 <template>
-  <div>
+  <div class="container">
+    <div class="item-data">
+      <img :src="itemData.image_path" alt="Memory Image" class="memory-image" />
+      <h2>{{ itemData.diary }}</h2>
+      <p>{{ itemData.memory_date }}</p>
+    </div>
     <iframe
       id="spotify-player"
       :src="`https://open.spotify.com/embed/track/${$route.params.musicid}`"
@@ -9,11 +14,6 @@
       allowtransparency="true"
       allow="encrypted-media"
     ></iframe>
-    <div>
-      <h2>{{ itemData.diary }}</h2>
-      <p>{{ itemData.memory_date }}</p>
-      <img :src="itemData.image_path" alt="Memory Image" />
-    </div>
     <UButton class="custom-button" @click="goToHomePage">ホームに戻る</UButton>
   </div>
 </template>
@@ -36,3 +36,33 @@ const goToHomePage = () => {
   router.push("/");
 };
 </script>
+
+<style scoped>
+.container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+  padding: 20px;
+}
+
+.item-data {
+  margin: 20px 0;
+}
+
+.memory-image {
+  width: 480px;
+  height: auto;
+  margin: 20px 0;
+}
+
+.custom-button {
+  margin-top: 20px;
+  color: #fff;
+  border: none;
+  padding: 10px 20px;
+  border-radius: 5px;
+  cursor: pointer;
+}
+</style>
